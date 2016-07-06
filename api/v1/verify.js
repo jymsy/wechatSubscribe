227c04c7c319d6a69c73010d90b765dd92effb97
jymsy <jym20083617@163.com>
2016-07-06 21:05:11 +0800

    update

--- api/v1/verify.js
+++ api/v1/verify.js
@@ -1,5 +1,19 @@
+var token = "jymsynb"
 
 exports.verify = function(req, res) {
-    var code = req.body.code;
-    res.json({data:"results"});
+    var query = req.query;
+    var signature = query.signature;
+    var timestamp = query.timestamp;
+    var nonce = query.nonce;
+    var echostr = query.echostr;
+    var sortArr = [token, timestamp, nonce];
+    var verifyStr = sortArr.sort().join('');
+
+    console.log(verifyStr);
+    if (verifyStr == signature) {
+        res.send(echostr);
+    } else {
+        res.send('error');
+    }
+    
 };
