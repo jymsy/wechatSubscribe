db9d566b6aebcccd5b3bf30f8e50c325b445e57a
jymsy <jym20083617@163.com>
2016-07-12 19:05:10 +0800

    update

--- define.js
+++ define.js
@@ -6,5 +6,7 @@ module.exports = {
     appId: 'wxa9a67cd8b04081ae',
     appSecret: '3d20db4745cf0836b995a5c311371055',
     tokenUrl: 'https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=APPID&secret=APPSECRET',
-    sendmsgUrl: 'https://api.weixin.qq.com/cgi-bin/message/mass/sendall?access_token=ACCESS_TOKEN'
+    sendmsgUrl: 'https://api.weixin.qq.com/cgi-bin/message/mass/sendall?access_token=ACCESS_TOKEN',
+    testUid: 'oV4ZTwUq3ATzRUjBSUC2btdj20ik',
+    templateId: '7R_8wxg7wapjSdl1OTN3BcTNnO9HrYq9hifInsgsdU4'
 };
--- gome.js
+++ gome.js
@@ -1,35 +1,49 @@
 var https = require('https');
 var _ = require('lodash');
-var accessToken = require('./components/accessToken');
+var config = require('./define');
+var WechatAPI = require('wechat-api');
 
-var start
 var url = 'https://www.gomemyc.com/api/v2/loans/getLoanWithPage?&pageSize=6&status=SCHEDULED&minDuration=0&maxDuration=3&minRate=0&maxRate=100&currentPage=1&product=';
 
+var getData = function(name, time) {
+  return {
+    name: {
+      value: name
+    },
+    time: {
+      value: time
+    }
+  };
+};
 
-setInterval(function() {
-	https.get(url, function(res) {
-		var results = '';
-		res.on('data', function(chunk) {
-			results += chunk;
-		});
-		res.on('end', function() {
-			var loans = JSON.parse(results);
-			_.forIn(loans.results, function(value) {
-				if (value.rate == 550 &&
-					(value.status == 'SCHEDULED' || value.status == 'OPENED')) {
-					var timeOpen = new Date(value.timeOpen);
-					var date = timeOpen.getHours() + ':' + timeOpen.getMinutes() + ':' + timeOpen.getSeconds();
-					console.log(value.title + ' ' + date);
-					accessToken.getAccessToken(function(token) {
-						if (token) {
-							console.log(token);
-						}
-					});
-				}
-			});
-		});
-	}).on('error', function(e) {
-		console.error(e);
-	});
+var checkGome = function() {
+    https.get(url, function(res) {
+    var results = '';
+    res.on('data', function(chunk) {
+      results += chunk;
+    });
+    res.on('end', function() {
+      var loans = JSON.parse(results);
+      _.forIn(loans.results, function(value) {
+        if (value.rate == 550 &&
+          (value.status == 'SCHEDULED' || value.status == 'OPENED')) {
+          var timeOpen = new Date(value.timeOpen);
+          var date = timeOpen.getHours() + ':' + timeOpen.getMinutes() + ':' + timeOpen.getSeconds();
+          console.log(value.title + ' ' + date);
+          var api = new WechatAPI(config.appId, config.appSecret);
+          api.sendTemplate(config.testUid, config.templateId, '', getData(value.title, date), function(err, result) {
+              console.log(result);
+          });
+        }
+      });
+    });
+  }).on('error', function(e) {
+    console.error(e);
+  });
+}
 
-}, 60000);
+checkGome();
+
+setInterval(checkGome, 60000);
+
+
