23582c1fc89382be58f8eac3b163a544f344ac49
jymsy <jym20083617@163.com>
2018-05-31 11:19:35 +0800

    update

--- api_router_v1.js
+++ api_router_v1.js
@@ -1,4 +1,4 @@
-
+let detailItemController = require('./api/v1/detailItem');
 var verifyController    = require('./api/v1/verify');
 var sendMsgController = require('./api/v1/sendmsg');
 
@@ -6,4 +6,5 @@ var router            =  require('express').Router();
 
 router.get('/verify', verifyController.verify);
 router.get('/sendmsg', sendMsgController.sendmsg);
+router.get('/createItem', detailItemController.create);
 module.exports = router;
